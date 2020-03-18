15784864ee8a6ed599fd0200194d81dd93da704c
Kevin Leung <kevin.leung987@hotmail.com>
2020-03-17 20:01:03 -0400

    fix toronto button

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
 // DOM Elements
 const resultField = document.querySelector("#result");
+const clearTipButton = document.querySelector("#clear-tip");
 const torontoButton = document.querySelector("#toronto-button");
 const sanFranButton = document.querySelector("#sanfran-button");
-const taxInput = document.querySelector('#tax-input');
+const taxInput = document.querySelector("#tax-input");
+const tipInput = document.querySelector("#tip-input");
 // State Variables
 let taxValue = 13;
+let tipValue = 12;
 // Update Functions
-const updateTaxInput = (value) => {
+const updateTaxInput = value => {
   taxValue = value;
   taxInput.value = taxValue;
+};
+const updateTipInput = value => {
+  tipValue = value;
+  tipInput.value = tipValue;
 }
 // Event Listeners
+clearTipButton.addEventListener('click', () => {
+  updateTipInput(0);
+})
 torontoButton.addEventListener("click", () => {
-  updateTaxInput(13)
+  updateTaxInput(13);
 });
 sanFranButton.addEventListener("click", () => {
-  updateTaxInput(8.5)
+  updateTaxInput(8.5);
 });
 // Setup
 resultField.innerHTML = "You owe: $6.33";
