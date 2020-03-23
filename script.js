aa0a846b0ea08c9605aa7f2c442e8c97cf1a4a47
Kevin Leung <kevin.leung987@hotmail.com>
2020-03-23 15:19:35 -0400

    button functionality

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const torontoButton = document.querySelector("#toronto-button");
 const sanFranButton = document.querySelector("#sanfran-button");
 const taxInput = document.querySelector("#tax-input");
 const tipInput = document.querySelector("#tip-input");
+const venmoButton = document.querySelector("#venmo");
+const interacButton = document.querySelector("#interac");
 // State Variables
 let taxValue = 13;
 let tipValue = 12;
@@ -22,6 +24,14 @@ const updateInput = (data, updateFunc, parseFunc) => {
   const parsed = parseFunc(data);
   updateFunc(!isNaN(parsed) ? parsed : 0);
 };
+// Utility
+const writeToClipboard = text => {
+  navigator.permissions.query({ name: "clipboard-write" }).then(result => {
+    if (result.state == "granted" || result.state == "prompt") {
+      navigator.clipboard.writeText(text);
+    }
+  });
+};
 // Event Listeners
 taxInput.addEventListener("blur", e => {
   updateInput(e.target.value, setTax, parseFloat);
@@ -37,6 +47,12 @@ torontoButton.addEventListener("click", () => {
 });
 sanFranButton.addEventListener("click", () => {
   setTax(8.5);
+});
+venmoButton.addEventListener("click", () => {
+  writeToClipboard("@kevleung98"); // TODO: Snackbar
+});
+interacButton.addEventListener("click", () => {
+  writeToClipboard("kevin.leung987@hotmail.com");
 });
 // Setup
 resultField.innerHTML = "You owe: $6.33";
